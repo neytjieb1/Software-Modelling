bcda51adc988f8d618e0be0892014b72894963da
Jo <u17091820@tuks.co.za>
2020-09-09 17:52:46 +0200

    Before Making changes

---
+++ Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/BaseImage.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/BaseImage.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/ImageAddOns.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/ImageAddOns.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/ImageElement.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/ImageElement.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/LevelFiveState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/LevelFiveState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/LevelFourState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/LevelFourState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/LevelOneState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/LevelOneState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/LevelState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/LevelState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/LevelThreeState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/LevelThreeState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/LevelTwoState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/LevelTwoState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/Mask.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/Mask.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/NoLevelState.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/NoLevelState.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/Pandemic.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/Pandemic.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/PandemicStrategy.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/PandemicStrategy.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/Poster.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/Poster.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/TemperatureCheck.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/TemperatureCheck.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/UseHandSanitiser.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/UseHandSanitiser.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/WashHands.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/WashHands.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/levelFiveStrategy.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/levelFiveStrategy.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/levelFourStrategy.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/levelFourStrategy.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/levelOneStrategy.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/levelOneStrategy.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/levelThreeStrategy.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/levelThreeStrategy.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/levelTwoStrategy.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/levelTwoStrategy.cpp.o"
+  "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/main.cpp" "/home/jo/CLionProjects/Software-Modelling/Assignments/A2/cmake-build-debug/CMakeFiles/A2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Assignments/A2/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ Assignments/A2/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -48,5 +48,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/A.dir/DependInfo.cmake"
+  "CMakeFiles/A2.dir/DependInfo.cmake"
   )
